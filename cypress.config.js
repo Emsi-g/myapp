34a1def537bd5e2c8d23ae3cadcf8dd21bb1ae89
Emsi-G <marcchristianp.gregorio@gmail.com>
2023-10-26 13:49:14 +0800

    Working

--- cypress.config.js
+++ cypress.config.js
@@ -5,23 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // inside config.browsers array each object has information like
-      // {
-      //   name: 'chrome',
-      //   channel: 'canary',
-      //   family: 'chromium',
-      //   displayName: 'Canary',
-      //   version: '80.0.3966.0',
-      //   path:
-      //    '/Applications/Canary.app/Contents/MacOS/Canary',
-      //   majorVersion: 80
-      // }
-      return {
-        browsers: config.browsers.filter(
-          (b) => b.family === 'chromium' && b.name !== 'electron'
-        ),
-      };
     },
-    // Add a browser setting here, for example:
-    browser: "chrome", // You can specify the browser you want to use
   },
 });
--- cypress/integration/first_test.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-it('Navigate product and review', function() {
-    cy.visit('http://localhost:3000/')
-
-    cy.get('a[href*="product/review"]').click()
-
-    cy.url().should('include', '/product/review')
-})
