63875c68c2896b9d5a4a4d73aaa73df419ebb58f
Emsi-G <marcchristianp.gregorio@gmail.com>
2023-10-26 21:31:15 +0800

    Final

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+         defaultCommandTimeout: 5000,
+         chromeWebSecurity: false, // Set to false to disable web security for Chrome (if needed)
+         chromeExecutable: '/usr/bin/google-chrome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
