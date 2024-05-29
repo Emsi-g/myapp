340188ad5184904412e1d40c578b150b65360305
Emsi <40536569+Emsi-g@users.noreply.github.com>
2024-05-30 00:16:18 +0800

    Update and rename pullImages.sh to stop_server.sh

--- scripts/pullImages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region ap-northeast-3 | docker login --username AWS --password-stdin 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com
-docker pull 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
