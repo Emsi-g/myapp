66591064c87aedf15035ba014fc06cc4a804ff06
Emsi <40536569+Emsi-g@users.noreply.github.com>
2024-05-30 00:38:25 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+systemctl start docker
 aws ecr get-login-password --region ap-northeast-3 | docker login --username AWS --password-stdin 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com
 docker pull 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
 docker run -d -p 80:80 --name test-docker 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
