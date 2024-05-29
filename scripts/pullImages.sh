4b488b70546a2ad06e6fba6bc49fb7fe997e75ee
Emsi <40536569+Emsi-g@users.noreply.github.com>
2024-05-29 22:47:38 +0800

    Update pullImages.sh

--- scripts/pullImages.sh
+++ scripts/pullImages.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-northeast-3 | docker login --username AWS --password-stdin 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com
-docker stop \`docker ps -q -a\`
 docker pull 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
