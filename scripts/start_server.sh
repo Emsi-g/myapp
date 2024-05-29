e515800b6b03505e7fe9a0b9486d766a067b11d2
Emsi <40536569+Emsi-g@users.noreply.github.com>
2024-05-29 22:41:14 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker pull 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
+docker run -d -p 80:80 --name test-docker 083341655278.dkr.ecr.ap-northeast-3.amazonaws.com/mc-aws-codepipeline:latest
